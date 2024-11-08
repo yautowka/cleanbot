91d80d5b77d7d894a010f5c180f182455cbdd8d3
Aleksey <51873777+yautowka@users.noreply.github.com>
2024-11-08 19:13:46 +0300

    Update android_release.sh

--- android_release.sh
+++ android_release.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+cd fluffychat
 cd android
 echo $FDROID_KEY | base64 --decode --ignore-garbage > key.jks
 echo "storePassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyAlias=key" >> key.properties
-echo "storeFile=../fluffychat/key.jks" >> key.properties
+echo "storeFile=../key.jks" >> key.properties
 echo $PLAYSTORE_DEPLOY_KEY >> keys.json
 ls | grep key
 bundle install

d16db54db00276b0137f8a5c71ad78426f471bc6
Aleksey <51873777+yautowka@users.noreply.github.com>
2024-11-15 18:09:18 +0300

    Update android_release.sh

--- android_release.sh
+++ android_release.sh
@@ -5,10 +5,5 @@ echo $FDROID_KEY | base64 --decode --ignore-garbag > key.jks
 echo "storePassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyAlias=key" >> key.properties
-echo "storeFile=key.jks" >> key.properties
-echo $PLAYSTORE_DEPLOY_KEY >> keys.json
-ls | grep key
-bundle install
-bundle update fastlane
-bundle exec fastlane set_build_code_internal
-cd ..
+echo "storeFile=../key.jks" >> key.properties
+#echo $PLAYSTORE_DEPLOY_KEY >> keys.json

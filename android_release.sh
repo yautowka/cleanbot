65e01d4e4595baa8bd1b7753dcece3fe8592b3a9
Aleksey <51873777+yautowka@users.noreply.github.com>
2024-11-15 18:22:55 +0300

    Update android_release.sh

--- android_release.sh
+++ android_release.sh
@@ -4,6 +4,6 @@ cd android
 echo $FDROID_KEY | base64 --decode --ignore-garbag > key.jks
 echo "storePassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties
-echo "keyAlias=key" >> key.properties
+echo "keyAlias=my-alias" >> key.properties
 echo "storeFile=../key.jks" >> key.properties
 #echo $PLAYSTORE_DEPLOY_KEY >> keys.json

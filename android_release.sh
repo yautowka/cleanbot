7fddb607f5653cf9ac1ed955218733c6f78d9364
Aleksey <51873777+yautowka@users.noreply.github.com>
2024-11-15 12:42:00 +0300

    Update android_release.sh

--- android_release.sh
+++ android_release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd fluffychat
 cd android
-echo $FDROID_KEY > key.jks
+echo $FDROID_KEY | base64 --decode --ignore-garbag > key.jks
 echo "storePassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyPassword=${FDROID_KEY_PASS}" >> key.properties
 echo "keyAlias=key" >> key.properties
